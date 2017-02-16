83432472199a8f367e1bf7e0c36d70c909ac75d4
Nicolas Gouzy <nicolas.gouzy@orange.com>
2017-02-16 11:11:47 +0100

    test(refactor): git command

--- test/integration/test_gitusage.py
+++ test/integration/test_gitusage.py
@@ -2,8 +2,18 @@ import os
 import shutil
 import subprocess
 import tempfile
+from typing import List
 
 from . import *
+
+
+def git_command(*git_args: List) -> subprocess.CompletedProcess:
+    args = ['git'] + list(git_args)
+    completed_process = subprocess.run(args,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE,
+                                       encoding="utf-8")
+    return completed_process
 
 
 # noinspection PyShadowingNames
@@ -14,37 +24,35 @@ def temp_dir(commitmsg_script):
         sample_file.write("sample content")
     old_cwd = os.getcwd()
     os.chdir(temporary_directory)
-    completed_process = subprocess.run(['git', 'init'],
-                                       stderr=subprocess.PIPE,
-                                       encoding="utf-8")
+
+    completed_process = git_command('init')
     assert completed_process.returncode == 0
     assert len(completed_process.stderr) == 0
+
+    completed_process = git_command('config', 'user.name', 'Nicolas Gouzy')
+    assert completed_process.returncode == 0
+    assert len(completed_process.stderr) == 0
+
     git_hook_commit_msg_path = os.path.join(temporary_directory, ".git", "hooks", "commit-msg")
     shutil.copy(commitmsg_script, git_hook_commit_msg_path)
-    completed_process = subprocess.run(['git', 'add', '.'],
-                                       stderr=subprocess.PIPE,
-                                       encoding="utf-8")
+    completed_process = git_command('add', '.')
     assert completed_process.returncode == 0
     assert len(completed_process.stderr) == 0
+
     yield None
+
     os.chdir(old_cwd)
 
 
 @pytest.mark.usefixtures("temp_dir")
 def test_git_commit_with_right_msg():
-    completed_process = subprocess.run(['git', 'commit', '-m', 'feat(ui): sample'],
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE,
-                                       encoding="utf-8")
+    completed_process = git_command('commit', '-m', 'feat(ui): sample')
     assert completed_process.returncode == 0
     assert len(completed_process.stderr) == 0
 
 
 @pytest.mark.usefixtures("temp_dir")
 def test_git_commit_with_wrong_msg():
-    completed_process = subprocess.run(['git', 'commit', '-m', 'wrong commit message'],
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE,
-                                       encoding="utf-8")
+    completed_process = git_command('commit', '-m', 'wrong commit message')
     assert completed_process.returncode != 0
     assert len(completed_process.stderr) > 0
